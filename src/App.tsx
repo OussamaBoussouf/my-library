9bd11ec94ea49eb185da49267205ebbf004f1968
Oussama Bof <jackmarrow06@gmail.com>
2024-05-19 22:34:14 +0200

    file structure

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { Button} from '@mui/material'
+import './App.css'
+
+
+function App() {
+
+  return (
+    <>
+     <h1 className='text-3xl font-bold underline'>Hello world</h1>
+     <Button variant="contained"> Click me</Button>
+    </>
+  )
+}
+
+export default App
---
+++ src/firestore.ts
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCH2MKUF3Kb1HydlLKykV3s9m60q99aNWo",
+  authDomain: "library-93f37.firebaseapp.com",
+  projectId: "library-93f37",
+  storageBucket: "library-93f37.appspot.com",
+  messagingSenderId: "983479564179",
+  appId: "1:983479564179:web:a67db6bb90f391cadf1a66"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
